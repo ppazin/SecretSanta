fa85d8b9840bf2b8a2d2415b59d38694f04d0a19
Paula Pažin <ppazin22@foi.hr>
2024-11-06 18:13:41 +0100

    Created class osoba

---
+++ SecretSanta/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta
+{
+    public class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        public Osoba(string ime, string prezime)
+        {
+            Ime = ime;
+            Prezime = prezime;
+        }
+    }
+}
